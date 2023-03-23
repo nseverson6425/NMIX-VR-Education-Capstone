4bbba6c469b4955a9571189b23cb6aa1c7f3522a
iskerop <oscar.parada2111@gmail.com>
2023-03-23 15:29:25 -0400

    transfer update

---
+++ Education App/Assets/ProjectileController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this object will be a bomb that explodes when it hits an answer
+//otherwise it will disappear if it misses its target and then respawn a new bomb/projectile that the user can use
+
+public class ProjectileController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
